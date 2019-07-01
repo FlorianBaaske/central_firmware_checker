a42bed9805013131b6604803b1bfe40f515c285b
FlorianBaaske <flo@flomain.de>
2019-07-01 11:40:15 +0200

    new version
    Added user authentication so we do not need the refresh token file anymore.

--- firmware-checker.py
+++ firmware-checker.py
@@ -2,6 +2,7 @@ import requests
 import json
 import os
 import smtplib
+import atexit
 from optparse import OptionParser
 from tabulate import tabulate
 
@@ -9,12 +10,26 @@ from tabulate import tabulate
 
 #Base URL for Central API Gateway
 base_url = ''
+#Central Customer ID
+customerID = ''
 #Client ID from the Central API Gateway configuration
 client_id = ''
 #Client Secret from the Central API Gateway configuraion
 client_secret = ''
 #Refresh Token from the Central API Gateway configuration
 refresh_token = ''
+##Central Username
+username = ''
+#Central User Password
+password = ''
+#Central CSRF Token
+csrf = ''
+#Central Session
+csession = ''
+#Central Authentication Code
+authCode = ''
+#Central Access Token
+access_token = ''
 grant_type = 'refresh_token'
 
 #Global Mail Server Options
@@ -34,9 +49,12 @@ mail_receiver = ''
 
 parser = OptionParser()
 parser.add_option('--base_url', dest='base_url', help='Base URL for Central API Gateway')
+parser.add_option('--customer_id', dest='customerID', help='Your Customer ID from Central')
 parser.add_option('--client_id', dest='client_id', help='Client ID from the Central API Gateway configuration')
 parser.add_option('--client_secret', dest='client_secret', help='Client Secret from the Central API Gateway configuraion')
 parser.add_option('--refresh_token', dest='refresh_token', help='Refresh Token from the Central API Gateway configuration')
+parser.add_option('--central_username', dest='username', help='Central Username')
+parser.add_option('--central_password', dest='password', help='Central password')
 
 parser.add_option('--mail_server', dest='mail_server', help='hostname or IP of the mail server')
 parser.add_option('--mail_server_port', dest='mail_server_port', help='Port of the Mailserver')
@@ -49,12 +67,18 @@ parser.add_option('--mail_receiver', dest='mail_receiver', help='Receiver addres
 
 if (options.base_url):
     base_url = options.base_url
+if (options.customerID):
+    customerID = options.customerID
 if (options.client_id):
     client_id = options.client_id
 if (options.client_secret):
     client_secret = options.client_secret
 if (options.refresh_token):
     refresh_token = options.refresh_token
+if (options.username):
+    username = options.username
+if (options.password):
+    password = options.password
 
 if (options.mail_server):
     mail_server = options.mail_server
@@ -68,6 +92,37 @@ if (options.mail_sender):
     mail_sender = options.mail_sender
 if (options.mail_receiver):
     mail_receiver = options.mail_receiver
+
+
+def userAuthentication():
+    global username
+    global password
+    global csrf
+    global csession
+
+    url = '/oauth2/authorize/central/api/login'
+    oauthURL = base_url + url
+    params = {'client_id': client_id}
+    headers = {'Content-type': 'application/json'}
+
+    if len(username) == 0:
+        print('username:', end='')
+        username = input()
+
+    if len(password) == 0:
+        print('password:', end='')
+        password = input()
+
+    payload = {'username': username, 'password': password}
+
+    resp = requests.post(oauthURL, params=params, data=json.dumps(payload), headers=headers)
+
+    print(resp.url)
+    print(resp)
+
+    csrf = resp.cookies['csrftoken']
+    csession = resp.cookies['session']
+
 
 
 def send_mail(my_list):
@@ -92,16 +147,20 @@ def check_error(response):
         return True
     return False
 
-def api_call_post(url, payload):
-    response = requests.post(url, payload)
-    json_data = json.loads(response.text)
-    if check_error(json_data):
-        print(response.url)
-        print(response.reason)
-        print(response.request)
-        print(response.status_code)
-        exit(0)
-    return json_data
+def postRequest(url, payload, params):
+
+    postURL = base_url + url
+
+    sesk = 'session=' + csession
+
+    headers = {'X-CSRF-TOKEN':csrf, 'Content-type': 'application/json', 'Cookie':sesk}
+
+    resp = requests.post(postURL, params=params, data=json.dumps(payload), headers=headers)
+    print(resp.url)
+    print(resp)
+
+    return resp.json()
+
 
 def api_call_get(url, payload):
     response = requests.get(url, payload)
@@ -113,27 +172,40 @@ def api_call_get(url, payload):
         print(response.status_code)
         exit(0)
     return json_data
-if not refresh_token:
-    if os.path.isfile('token.json'):
-        with open ('token.json', 'r') as token_file:
-            if os.path.getsize('token.json') > 0:
-                data = json.load(token_file)
-                refresh_token = data['refresh_token']
-        token_file.close()
-if not refresh_token:
-    exit(0)
 
-payload = {'client_id' : client_id, 'client_secret' : client_secret, 'grant_type': grant_type, 'refresh_token' : refresh_token}
+def deleteRquest(url, payload):
+    deleteURL = base_url + url
+    
+    payload['access_token'] = access_token 
+    sesk = 'session=' + csession
 
-url = base_url + '/oauth2/token'
+    headers = {'X-CSRF-TOKEN':csrf, 'Content-type': 'application/json', 'Cookie':sesk}
 
-response = api_call_post(url, payload)
+    if url == '/oauth2/api/tokens':
+        resp = requests.delete(deleteURL, data=json.dumps(payload), headers=headers)
+    else:
+        resp = requests.delete(deleteURL, params=payload)
 
-with open('token.json', 'w') as token_file:
-    json.dump(response, token_file)
-token_file.close()
+    print(resp.url)
+    print(resp)
 
-access_token = response['access_token']
+def cleanup():
+    print('clean UP')
+    deleteRquest('/oauth2/api/tokens', {'customer_id': customerID})
+    
+
+atexit.register(cleanup)
+
+print('authenticate user')
+userAuthentication()
+
+print ('get authentication code')
+data = postRequest('/oauth2/authorize/central/api', {'customer_id': customerID}, {'client_id': client_id, 'response_type': 'code', 'scope': 'all'})
+authCode = data['auth_code']
+
+print('get access token')
+data = postRequest('/oauth2/token', {'customer_id': customerID}, {'client_id': client_id, 'grant_type': 'authorization_code', 'client_secret': client_secret, 'code': authCode})
+access_token = data['access_token']
 
 url = base_url + '/firmware/v1/devices'
 device_type = {'HP', 'MAS', 'CONTROLLER'}
